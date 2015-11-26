51cc2576a423cc1001b55d000bca45cba5e33b91
xiangleilei <xiangleilei@12308.com>
2015-11-26 09:38:45 +0800

    d

--- app/gx84100/views.py
+++ app/gx84100/views.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 
 # from app.gx84100 import gx84100
-
+from lxml import etree
 import requests
 
 # @gx84100.route('/', methods=['GET', 'POST'])
@@ -15,7 +15,7 @@ def getTrainInfo():
 
     url = 'http://www.84100.com/getTrainInfo/ajax'
     payload = {
-        "shiftId": 91730575 ,   
+        "shiftId": 92069979 ,   
         "startId": 45010000 ,
         "startName": u"埌东站",    
         "ttsId": ''  
@@ -23,7 +23,53 @@ def getTrainInfo():
 
     trainInfo = requests.post(url, data=payload)
     
-    print trainInfo.json()
+    trainInfo = trainInfo.json()
+    yupiao=0
+    if str(trainInfo['flag'])=='0':
+        sel = etree.HTML(trainInfo['msg'])
+        yupiao= sel.xpath('//div[@class="ticketPrice"]/ul/li/strong[@id="leftSeatNum"]/text()')
+        if yupiao:
+            yupiao = int(yupiao[0])
+        
+    
+# getTrainInfo()  
+
+
+
+
+
+def test():
     
     
-getTrainInfo()  
+    
+    headers={"cookie":'JSESSIONID=21CB50BE34B16A49661D21F0C1E85660'} 
+    
+    
+    
+    
+    r = requests.get('http://www.84100.com/user/getPeople/ajax?id=171715', headers=headers)  #huoqulianxirenxiangqing
+    #r = requests.get('http://www.84100.com/user/getPeople/ajax?id=171715')
+    
+    
+
+    content = r.content
+    print content
+
+
+test()
+
+def xiadan():
+    
+    
+    # startId    45010003
+    # name    向磊磊
+    # mobile    13267109876
+    # planId    92118664
+    # ticketNo    
+    # ticketPassword    
+    # idTypes    1,1
+    # idNos    429006199012280042,429006198906100034
+    # names    李梦蝶,向磊磊
+    # mobiles    ,
+    # ticketTypes    全票,全票
+    pass
