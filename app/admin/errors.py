a75bf0c9cbf7ef8e9cbe68304c9aeeae4c2625c9
luocky <luojunping@qq.com>
2015-12-03 15:05:53 +0800

    admin

--- app/admin/errors.py
+++ app/admin/errors.py
@@ -5,11 +5,11 @@ from app.admin import admin
 from flask import jsonify
 
 
-@admin.app_errorhandler(404)
-def page_not_found(e):
-    return jsonify({"code": RET_PAGE_404, "message": "page not found", "data": ""})
-
-
-@admin.app_errorhandler(500)
-def internal_server_error(e):
-    return jsonify({"code": RET_SERVER_ERROR, "message": "服务器异常", "data": ""})
+#@admin.app_errorhandler(404)
+#def page_not_found(e):
+#    return jsonify({"code": RET_PAGE_404, "message": "page not found", "data": ""})
+#
+#
+#@admin.app_errorhandler(500)
+#def internal_server_error(e):
+#    return jsonify({"code": RET_SERVER_ERROR, "message": "服务器异常", "data": ""})
--- app/admin/views.py
+++ app/admin/views.py
@@ -1,30 +1,47 @@
 # -*- coding:utf-8 -*-
+import math
 
+from flask import render_template, request
+from flask.views import MethodView
 from app.admin import admin
-from flask import render_template
-from app.models import Order
+from app.models import Order, Line
 
 def parse_page_data(qs):
-    total=qs.count()
-    page=self.get_param("page",TYPE_INT,default=1)
+    total = qs.count()
     page = int(request.args.get("page", default=1))
     pageCount=int(request.args.get("pageCount", default=10))
     pageNum=int(math.ceil(total*1.0/pageCount))
-    return Dict({
+    skip = (page-1)*pageCount
+    range_min = max(1, page-5)
+    range_max = min(range_min+10, pageNum)
+    return {
         "total":total,
         "pageCount":pageCount,
         "pageNum":pageNum,
         "page":page,
-        "skip":(page-1)*pageCount,
+        "skip": skip,
         "previous":page-1,
         "next":page+1,
-        })
+        "range": range(range_min, range_max),
+        "items": qs[skip: skip+pageCount]
+        }
 
 @admin.route('/', methods=['GET'])
-def index():
-    orders = Order.objects
-    kwargs = {
-        "page": parse_page_data,
-        "orders": orders,
-    }
-    return render_template('admin/order_list.html', **kwargs)
+@admin.route('/orders', methods=['GET'])
+def order_list():
+    return render_template('admin/order_list.html', page=parse_page_data(Order.objects))
+
+@admin.route('/lines', methods=['GET'])
+def line_list():
+    return render_template('admin/line_list.html', page=parse_page_data(Line.objects))
+
+
+class SubmitOrder(MethodView):
+    def get(self):
+        return render_template('admin/submit_order.html', item=None)
+
+    def post(self):
+        pass
+
+
+admin.add_url_rule("/submit_order", view_func=SubmitOrder.as_view('submit_order'))
