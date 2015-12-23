827a1796e58c88c483c5e278db3c713c2e11199b
luocky <luojunping@12308.com>
2015-12-23 21:31:53 +0800

    log

--- app/__init__.py
+++ app/__init__.py
@@ -15,6 +15,8 @@ from celery import Celery, platforms
 from redis_session import RedisSessionInterface
 platforms.C_FORCE_ROOT = True    # celery需要这样
 from raven.contrib.flask import Sentry
+from logging.handlers import TimedRotatingFileHandler
+from logging import Formatter, StreamHandler
 
 mail = Mail()
 db = MongoEngine()
@@ -22,6 +24,11 @@ celery = Celery(__name__, broker="redis://localhost:6379/10")
 login_manager = LoginManager()
 BASE_DIR = os.path.split(os.path.abspath(os.path.dirname(__file__)))[0]
 sentry = Sentry()
+
+line_log = logging.getLogger("line")
+order_log = logging.getLogger("order")
+kefu_log = logging.getLogger("kefu")
+access_log = logging.getLogger("access")
 
 
 def init_celery(app):
@@ -37,6 +44,23 @@ def init_celery(app):
     celery.Task = ContextTask
 
 
+def init_logging(app, server_type):
+    fmt = Formatter('[%(asctime)s] %(levelname)s: %(message)s')
+    stdout_fhd = StreamHandler()
+    stdout_fhd.setLevel(logging.DEBUG)
+    stdout_fhd.setFormatter(fmt)
+    for logger in [line_log, order_log, kefu_log, access_log]:
+        logger.setLevel(logging.DEBUG)
+        s = logger.name
+        f = "logs/%s_%s.log" % (s, server_type)
+        file_hd = TimedRotatingFileHandler(os.path.join(BASE_DIR, f),
+                                           when='D', interval=1)
+        file_hd.setLevel(logging.INFO)
+        file_hd.setFormatter(fmt)
+        logger.addHandler(stdout_fhd)
+        logger.addHandler(file_hd)
+
+
 def setup_app(config_name, server_type="api"):
     servers = {
         "api": setup_api_app,
@@ -49,6 +73,7 @@ def setup_app(config_name, server_type="api"):
     app.session_interface = RedisSessionInterface(redis=r)
 
     sentry.init_app(app)
+    init_logging(app, server_type)
     return app
 
 
