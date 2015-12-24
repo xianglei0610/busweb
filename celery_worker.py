5b57a3d4698a31bf20296658a849cbb86d17aadf
luocky <luojunping@12308.com>
2015-12-24 09:39:33 +0800

    celery sentry support

--- celery_worker.py
+++ celery_worker.py
@@ -5,6 +5,21 @@ import sys
 reload(sys)
 sys.setdefaultencoding("utf-8")
 from app import setup_app, celery
+from raven import Client
+from raven.contrib.celery import register_signal, register_logger_signal
 
 app = setup_app(os.getenv('FLASK_CONFIG') or 'local',
                 os.getenv('FLASK_SERVER') or 'api')
+
+
+dsn = app.config["CELERY_SENTRY_DSN"]
+if dsn:
+    client = Client(dsn)
+    # register a custom filter to filter out duplicate logs
+    register_logger_signal(client)
+    # hook into the Celery error handler
+    register_signal(client)
+    # The register_logger_signal function can also take an optional argument
+    # `loglevel` which is the level used for the handler created.
+    # Defaults to `logging.ERROR`
+    register_logger_signal(client, loglevel=logging.INFO)
