cf2f7108b8595ee2fbc8715cbfb4f7af4f5ec995
luocky <luojunping@12308.com>
2016-01-08 09:37:56 +0800

    增加账号

--- app/email.py
+++ app/email.py
@@ -4,7 +4,6 @@ from app import mail
 
 
 def send_email(subject, sender, recipients, text_body, html_body):
-    return
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
