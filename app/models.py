e26929567b89f24c3c297e17de13c04a35c7c121
luocky <luojunping@12308.com>
2016-01-20 17:28:28 +0800

    -

--- app/models.py
+++ app/models.py
@@ -135,6 +135,7 @@ class Line(db.Document):
             "s_sta_name",
             "s_city_name",
             "d_city_name",
+            "d_city_code",
             "d_sta_name",
             "crawl_source",
             "drv_date",
