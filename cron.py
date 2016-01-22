2b546d828c53559282d68c8b5d0fa32f85421865
xiangleilei <xiangleilei@12308.com>
2016-01-22 15:45:37 +0800

    d

--- cron.py
+++ cron.py
@@ -148,9 +148,10 @@ def main():
 
     sched = Scheduler(daemonic=False)
 
+    sched.add_cron_job(bus_crawl, hour=22, minute=10, args=['baba'])
     sched.add_cron_job(bus_crawl, hour=12, minute=10, args=['bus100', "450000"]) #广西
-    sched.add_cron_job(bus_crawl, hour=14, minute=10, args=['bus100', "370000"]) #山东 
-#     sched.add_cron_job(bus_crawl, hour=16, minute=10, args=['bus100', "210000"]) #辽宁 
+    sched.add_cron_job(bus_crawl, hour=14, minute=10, args=['bus100', "370000"]) #山东
+#     sched.add_cron_job(bus_crawl, hour=16, minute=10, args=['bus100', "210000"]) #辽宁
     sched.add_cron_job(bus_crawl, hour=16, minute=10, args=['bus100', "410000"]) #河南
     sched.add_cron_job(sync_crawl_to_api, hour=18, minute=10, args=['bus100']) #同步爬虫数据到web
     sched.add_cron_job(del_people, hour=22, minute=40, args=['bus100']) #删除源站常用联系人
