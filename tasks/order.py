6c14cd7bea81b957ab7381491a8c34faba743072
luocky <luojunping@12308.com>
2016-01-28 14:41:32 +0800

    ticket.py与order.py合并

--- tasks/order.py
+++ tasks/order.py
@@ -1,12 +1,14 @@
 # -*- coding:utf-8 -*-
 import random
 import datetime
+import urllib2
+import json
 
 from app.constants import *
 from app import celery
 from app.utils import getRedisObj
 from app.email import send_email
-from app.flow import get_flow
+from app.flow import get_flow, get_compatible_flow
 from app.models import Order
 from app import order_log
 from flask import current_app
@@ -112,3 +114,76 @@ def check_order_completed(self, username, key, order_no, is_send=False):
                 pass
         if orderObj.status == STATUS_WAITING_ISSUE:
             self.retry(kwargs={"is_send": is_send},countdown=30+random.random()*10%3, max_retries=200)
+
+
+@celery.task(bind=True, ignore_result=True)
+def async_lock_ticket(self, order_no):
+    """
+    请求源网站锁票 + 锁票成功回调
+
+    Return:
+        expire_time: "2015-11-11 11:11:11",     # 订单过期时间
+        total_price: 322，          # 车票价格
+    """
+    order = Order.objects.get(order_no=order_no)
+    flow, new_line = get_compatible_flow(order.line)
+    order.modify(line=new_line)
+    flow.lock_ticket(order)
+
+
+@celery.task(ignore_result=True)
+def issued_callback(order_no):
+    """
+    出票回调
+
+    Return:
+    {
+        "code": RET_OK,
+        "message": "OK"
+        "data":{
+            "sys_order_no": "",
+            "out_order_no": "",
+            "raw_order_no"; "",
+            "pick_info":[{
+                "pick_code": "1",
+                "pck_msg": "2"
+            },],
+        }
+    }
+    """
+    from app.models import Order
+    order = Order.objects.get(order_no=order_no)
+    cb_url = order.issued_return_url
+    order_log.info("[issue-callback-start] order:%s, callback:%s", order_no, cb_url)
+    if not cb_url:
+        return
+    if order.status == STATUS_ISSUE_SUCC:
+        pick_info = []
+        for i, code in enumerate(order.pick_code_list):
+            pick_info.append({
+                "pick_code": code,
+                "pick_msg": order.pick_msg_list[i]
+            })
+        ret = {
+            "code": RET_OK,
+            "message": "OK",
+            "data": {
+                "sys_order_no": order.order_no,
+                "out_order_no": order.out_order_no,
+                "raw_order_no": order.raw_order_no,
+                "pick_info": pick_info,
+            }
+        }
+    else:
+        ret = {
+            "code": RET_ISSUED_FAIL,
+            "message": "fail",
+            "data": {
+                "sys_order_no": order.order_no,
+                "out_order_no": order.out_order_no,
+                "raw_order_no": order.raw_order_no,
+            }
+        }
+    order_log.info("[issue-callback] %s %s", order_no, str(ret))
+    response = urllib2.urlopen(cb_url, json.dumps(ret), timeout=10)
+    order_log.info("[issue-callback-response]%s %s", order_no, str(response))
