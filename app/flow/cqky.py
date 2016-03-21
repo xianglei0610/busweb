d87abd1c928ab3e644b183220dddd23dfa4094b0
luocky <luojunping@qq.com>
2016-03-21 23:43:53 +0800

    cqky line_id

--- app/flow/cqky.py
+++ app/flow/cqky.py
@@ -496,11 +496,12 @@ class Flow(BaseFlow):
             line_id_args = {
                 "s_city_name": line.s_city_name,
                 "d_city_name": line.d_city_name,
-                "bus_num": d["SchLocalCode"],
+                "s_sta_name": d["SchStationName"],
+                "d_sta_name": d["SchDstNodeName"],
                 "crawl_source": line.crawl_source,
                 "drv_datetime": drv_datetime,
             }
-            line_id = md5("%(s_city_name)s-%(d_city_name)s-%(drv_datetime)s-%(bus_num)s-%(crawl_source)s" % line_id_args)
+            line_id = md5("%(s_city_name)s-%(d_city_name)s-%(drv_datetime)s-%(s_sta_name)s-%(d_sta_name)s-%(crawl_source)s" % line_id_args)
             try:
                 obj = Line.objects.get(line_id=line_id)
             except Line.DoesNotExist:
