de094f3b53aadf0b5af5ef9f29e1f317abb13ac9
apple <pyserver@12308.com>
2016-04-15 21:09:45 +0800

    d

--- app/constants.py
+++ app/constants.py
@@ -1706,7 +1706,7 @@ PAY_ACCOUNTS = [
 
 DG_BANK = {
     "luoxiangyun": 'CMB', #BOCB2C:中国银行 CMB:招商银行 CCB :建设银行  SPABANK:平安银行
-    #"lf12308": 'SPABANK',
-    "lf12308": 'BOCB2C',
+    "lf12308": 'SPABANK',
+    #"lf12308": 'BOCB2C',
     "ck12308": 'CCB',
 }
