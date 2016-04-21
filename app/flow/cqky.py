9bd6c3a38b34598b92229e5dda2f47991662a318
banana <pyserver@12308.com>
2016-04-21 13:38:46 +0800

    -

--- app/flow/cqky.py
+++ app/flow/cqky.py
@@ -106,7 +106,7 @@ class Flow(BaseFlow):
                     if u"同一IP一天最多可订" in res["msg"]:
                         res["msg"] = "ip: %s %s" % (rebot.proxy_ip, res["msg"])
                         rebot.modify(ip="")
-                    elif u"当前用户今天交易数已满" in res["msg"] or u"当前登录用户已被列为可疑用户" in res["msg"] or u"当前系统维护中" in res["msg"]:
+                    elif u"当前用户今天交易数已满" in res["msg"] or u"当前登录用户已被列为可疑用户" in res["msg"]:
                         rebot.modify(cookies="{}")
                         rebot = order.change_lock_rebot()
                     lock_result.update({
@@ -120,6 +120,12 @@ class Flow(BaseFlow):
                      "result_code": 2,
                      "source_account": rebot.telephone,
                      "result_reason": u"账号未登录",
+                 })
+            elif u"当前系统维护中" in res["msg"]:
+                lock_result.update({
+                     "result_code": 2,
+                     "source_account": rebot.telephone,
+                     "result_reason": res["msg"],
                  })
             else:
                 lock_result.update({
