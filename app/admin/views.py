14dbf1cb62731cc3cfc218a29e01eb6d1e813c9e
luocky <luojunping@12308.com>
2016-04-21 11:40:10 +0800

    无线苏州不自动锁票

--- app/admin/views.py
+++ app/admin/views.py
@@ -616,7 +616,7 @@ def wating_deal_order():
                     continue
                 order.update(kefu_username=current_user.username)
                 assign.add_dealing(order, current_user)
-                if order.status == STATUS_WAITING_LOCK:
+                if order.status == STATUS_WAITING_LOCK and order.crawl_source not in [SOURCE_WXSZ]:
                     async_lock_ticket.delay(order.order_no)
                 push_kefu_order.apply_async((current_user.username, order.order_no))
     qs = assign.dealing_orders(current_user).order_by("create_date_time")
