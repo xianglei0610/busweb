6937bdea2bc3ecb617bad792dcb46c11fe2dcd24
luocky <luojunping@qq.com>
2016-05-01 13:15:58 +0800

    cqky: lock fail

--- app/flow/cqky.py
+++ app/flow/cqky.py
@@ -79,6 +79,7 @@ class Flow(BaseFlow):
                     u"可售数不足",
                     u"班次状态为保班",
                     u"无可售席位",
+                    u"中心转发30003请求TKLock_3失败"
                 ]
                 for s in lst:
                     if s in msg:
