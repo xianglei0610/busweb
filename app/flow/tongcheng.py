bbf537b14e6c5261ab25098e064a7b594392f9d3
luocky <luojunping@12308.com>
2016-05-18 10:48:55 +0800

    -

--- app/flow/tongcheng.py
+++ app/flow/tongcheng.py
@@ -128,7 +128,7 @@ class Flow(BaseFlow):
                 })
             else:
                 lock_result.update({
-                    "result_code": 0,
+                    "result_code": 2,
                     "result_reason": desc,
                     "source_account": rebot.telephone,
                 })
