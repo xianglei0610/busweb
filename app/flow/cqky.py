0b473ed8a1723e82773276bc59298e1d5025ed31
banana <pyserver@12308.com>
2016-05-25 22:27:36 +0800

    重庆例行维护不失败

--- app/flow/cqky.py
+++ app/flow/cqky.py
@@ -145,12 +145,12 @@ class Flow(BaseFlow):
                         "source_account": rebot.telephone,
                         "result_reason": "%s sta_mode:%s" % (res["msg"], mode),
                     })
-            elif "您未登录或登录已过期" in res["msg"]:
+            elif u"您未登录或登录已过期" in res["msg"] or u"例行维护" in res["msg"]:
                 rebot.modify(ip="")
                 lock_result.update({
                      "result_code": 2,
                      "source_account": rebot.telephone,
-                     "result_reason": u"账号未登录",
+                     "result_reason": res["msg"],
                  })
             else:
                 lock_result.update({
