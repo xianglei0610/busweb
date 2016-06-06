01b6d0a3b9a172751d6e9466c2220dba33cb8e96
luocky <luojunping@12308.com>
2016-06-06 11:45:54 +0800

    -

--- app/models.py
+++ app/models.py
@@ -1804,7 +1804,7 @@ class CqkyWebRebot(Rebot):
                               }):
             cnt = d["count"]
             phone = d["_id"]["phone"]
-            if cnt >= 7:
+            if cnt >= 5:
                 droped.add(phone)
         tele = random.choice(list(all_accounts-droped))
         return cls.objects.get(telephone=tele)
