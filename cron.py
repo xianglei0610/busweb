3c657b1c519e4b199c9539de811d3c7f5534b770
xiangleilei <xiangleilei@12308.com>
2016-06-07 15:20:52 +0800

    d

--- cron.py
+++ cron.py
@@ -308,7 +308,7 @@ def main():
     sched.add_cron_job(bus_crawl, hour=15, minute=0, args=['ctrip'], kwargs={"crawl_kwargs":{"province": "北京"}})
 
     #北京省网
-    sched.add_cron_job(bus_crawl, hour=16, minute=30, args=['bjky'])
+    sched.add_cron_job(bus_crawl, hour=8, minute=30, args=['bjky'])
 
     #唐山省网
     sched.add_cron_job(bus_crawl, hour=17, minute=10, args=['hebky'])
