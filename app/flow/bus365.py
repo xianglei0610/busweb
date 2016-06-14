9553a3f26e68901a4249adaa17d97688abbf7b4a
xiangleilei <xiangleilei@12308.com>
2016-06-14 11:30:51 +0800

    d

--- app/flow/bus365.py
+++ app/flow/bus365.py
@@ -243,11 +243,11 @@ class Flow(BaseFlow):
                 "result_code": 4,
                 "result_msg": order_status_mapping[state],
             })
-        elif state in (2, 3, 4, 5):#取消购票,购票失败,退票成功
-            result_info.update({
-                "result_code": 2,
-                "result_msg": order_status_mapping[state],
-            })
+#         elif state in (2, 3, 4, 5):#取消购票,购票失败,退票成功
+#             result_info.update({
+#                 "result_code": 2,
+#                 "result_msg": order_status_mapping[state],
+#             })
         return result_info
 
     def get_pay_page(self, order, valid_code="", session=None, pay_channel="alipay" ,**kwargs):
