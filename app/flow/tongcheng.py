104407336fb5f0b757ee1b8103bb1980f27fe97b
banana <pyserver@12308.com>
2016-06-22 20:19:57 +0800

    同程加服务费

--- app/flow/tongcheng.py
+++ app/flow/tongcheng.py
@@ -81,8 +81,8 @@ class Flow(BaseFlow):
                 "runThrough": "",
                 "AgentType": line.extra_info.get("AgentType", 7),
                 "ExtraSchFlag": 0,
-                "serviceChargeID": 0,
-                "serviceChargePrice":0,
+                "serviceChargeID": line.extra_info.get("serviceChargeID", 0),
+                "serviceChargePrice": line.extra_info.get("serviceChargePrice", 0),
                 "isHomeDelivery":0,
                 # "$$hashKey": "09X",
                 "$$hashKey": "017",
@@ -100,7 +100,7 @@ class Flow(BaseFlow):
             "stationCode": line.s_sta_id,
             "AlternativeFlag":0,
             "isSubscribe":False,
-            "count":1,
+            "count": order.ticket_amount,
             "coachType": line.vehicle_type,
             "activityId":0,
             "reductAmount":0,
@@ -112,9 +112,10 @@ class Flow(BaseFlow):
             "insuranceAmount":0,
             "voucherId":0,
             "voucherSellPrice":0,
-            "serviceChargeId":0,
-            "serviceChargeType":0,
-            "serviceChargeAmount":0,
+            "serviceChargeId": line.extra_info.get("serviceChargeID", 0),
+            "serviceChargeType": line.extra_info.get("serviceChargeType", 0),
+            "serviceChargeAmount": line.extra_info.get("serviceChargePrice", 0)*order.ticket_amount,
+            "serviceChargeUnitPrice": line.extra_info.get("serviceChargePrice", 0),
             "sessionId":"100362285"
         }
         url = "http://tcmobileapi.17usoft.com/bus/OrderHandler.ashx"
@@ -619,10 +620,16 @@ class Flow(BaseFlow):
 
             info = {
                 "full_price": float(d["ticketPrice"]),
-                "fee": 0,
+                "fee": float(d["serviceChargePrice"]),
                 "left_tickets": left,
                 "refresh_datetime": now,
-                "extra_info": {"AgentType":d["AgentType"], "timePeriodType":d[u'timePeriodType'], "runTime": d["runTime"]},
+                "extra_info": {
+                    "AgentType":d["AgentType"],
+                    "timePeriodType":d[u'timePeriodType'],
+                    "serviceChargeID": d["serviceChargeID"],
+                    "serviceChargePrice": d["serviceChargePrice"],
+                    "serviceChargeType": d["serviceChargeType"],
+                    "runTime": d["runTime"]},
             }
             if line_id == line.line_id:
                 update_attrs = info
