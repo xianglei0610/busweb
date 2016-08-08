4b985bcc92e8a5136f9f2d6fc63fe1a5aec18dd2
xiangleilei <xiangleilei@12308.com>
2016-08-08 17:17:55 +0800

    dgky

--- app/flow/dgky.py
+++ app/flow/dgky.py
@@ -42,19 +42,15 @@ class Flow(BaseFlow):
                "Host": "www.mp0769.com",
                }
         contact_name = order.contact_info["name"]
+        msg = ''
         if contact_name.isdigit():
             msg = u"姓名是数字，切换到广东省网下单"
-            order.modify(line=Line.objects.get(line_id=order.line.check_compatible_lines().get("gdsw", "")),crawl_source='gdsw')
-            lock_result.update({
-                "result_code": 2,
-                "source_account": '',
-                "result_reason":  msg
-            })
-            return lock_result
         try:
             order.contact_info["name"].decode('utf8').encode('gb2312')
         except:
             msg = u"汉字转换错误，切换到广东省网下单"
+
+        if msg:
             order.modify(line=Line.objects.get(line_id=order.line.check_compatible_lines().get("gdsw", "")),crawl_source='gdsw')
             lock_result.update({
                 "result_code": 2,
@@ -120,7 +116,17 @@ class Flow(BaseFlow):
                 left_tickets = params['ct_accnum']
                 end_station = params['ct_stname'].decode('gbk')
                 break
-        if float(full_price) > 0 and float(full_price) == float(order.order_price) :
+
+        if float(full_price) > 0:
+            if float(full_price)*order.ticket_amount != float(order.order_price):
+                msg = u"金额不一致"
+                order.modify(line=Line.objects.get(line_id=order.line.check_compatible_lines().get("gdsw", "")),crawl_source='gdsw')
+                lock_result.update({
+                    "result_code": 2,
+                    "source_account": '',
+                    "result_reason":  msg
+                })
+                return lock_result
             agree_url = sel.xpath('//form[@id="Form1"]/@action')[0]
             agree_url = "http://www.mp0769.com/" + agree_url
             data = urllib.urlencode(params)
