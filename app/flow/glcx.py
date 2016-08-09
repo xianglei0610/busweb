83572293529a2a9faff4203f82ae45cf9df33956
luocky <luojunping@12308.com>
2016-08-09 11:37:09 +0800

    山东交运, 余票刷新重写

--- app/flow/glcx.py
+++ app/flow/glcx.py
@@ -1,24 +1,19 @@
 # coding=utf-8
 
 import requests
-import re
-import time
 import json
 import urllib
 
 import datetime
 import random
 from bs4 import BeautifulSoup as bs
-# from PIL import Image
 
 from app.constants import *
 from datetime import datetime as dte
 from app.flow.base import Flow as BaseFlow
-from app.models import Line
+from app.models import Line, GlcxWebRebot
 from app.utils import md5, vcode_glcx
-from app import rebot_log, order_log
-# import cStringIO
-from time import sleep
+from app import order_log
 
 
 class Flow(BaseFlow):
@@ -57,12 +52,10 @@ class Flow(BaseFlow):
         #     line.extra_info['startNo'], line.full_price, \
         #     line.drv_date, line.drv_time)
         # pa = ''.join(pa.split())
-        # rebot_log.info(pa)
         # pa = urllib.quote(pa.encode('utf-8'), safe='=&+')
         # r = requests.post(url, headers=headers, cookies=cookies, data=pa)
         # soup = bs(r.content, 'lxml')
         # info = soup.find('div', attrs={'class': 'buy'})
-        # rebot_log.info(info)
         v = vcode_glcx(cookies)
         url = 'http://www.0000369.cn/buytks!toAffirm.action'
         pre = 'insuranceValue=2&'
@@ -86,18 +79,13 @@ class Flow(BaseFlow):
 
         pa = pre + tmp + suf
         pa = ''.join(pa.split())
-        # rebot_log.info(pa)
         pa = urllib.quote(pa.encode('utf-8'), safe='=&+')
         r = requests.post(url, headers=headers, cookies=cookies, data=pa)
         soup = bs(r.content, 'lxml')
-        # rebot_log.info(soup)
         try:
             sn = soup.find('input', attrs={'id': 'dealOrder'}).get('value', '')
         except:
             sn = ''
-        # rebot_log.info(cookies)
-        # rebot_log.info(soup.title)
-        # rebot_log.info(sn)
         if '确认支付' in soup.title and sn:
             expire_time = dte.now() + datetime.timedelta(seconds=15 * 60)
             # cookies = {}
@@ -189,7 +177,6 @@ class Flow(BaseFlow):
             result_info.update(result_msg="状态未变化")
             return result_info
         ret = self.send_order_request(order)
-        # rebot_log.info(ret)
         state = ret['state']
         if '失败' in state:
             result_info.update({
@@ -216,63 +203,59 @@ class Flow(BaseFlow):
             })
         return result_info
 
-    # 线路刷新, java接口调用
     def do_refresh_line(self, line):
         now = dte.now()
+        result_info = {}
         data = {
             'stationId': line.s_sta_id,
             'portId': line.d_sta_id,
             'startTime': line.drv_date,
         }
-        ua = random.choice(BROWSER_USER_AGENT)
+        rebot = GlcxWebRebot.get_one()
         headers = {
-            "User-Agent": ua,
+            "User-Agent": random.choice(BROWSER_USER_AGENT),
             'Content-Type': 'application/x-www-form-urlencoded',
             'X-Requested-With': 'XMLHttpRequest',
         }
         url = 'http://www.0000369.cn/buytks!searchtks.action'
-        r = requests.post(url, headers=headers, data=urllib.urlencode(data))
-        now = dte.now()
-        update_attrs = {}
-        ft = Line.objects.filter(s_city_name=line.s_city_name,
-                                 d_city_name=line.d_city_name, drv_date=line.drv_date)
-        t = {x.line_id: x for x in ft}
-        update_attrs = {}
-        soup = bs(r.content, 'lxml')
         try:
-            info = soup.find('table', attrs={'id': 'selbuy'})
-            items = info.find_all('tr', attrs={'class': True})
+            r = rebot.http_post(url, headers=headers, data=urllib.urlencode(data))
+            soup = bs(r.content, 'lxml')
         except:
-            result_info = {}
             result_info.update(result_msg="exception_ok", update_attrs={"left_tickets": 5, "refresh_datetime": now})
             return result_info
-        for x in items:
-            try:
-                y = x.find_all('td')
-                bus_num = y[0].get_text().strip()
-                full_price = y[6].get_text().strip()
-                left_tickets = y[10].get_text().strip()
-                line_id_args = {
-                    's_city_name': line.s_city_name,
-                    'd_city_name': line.d_city_name,
-                    'crawl_source': line.crawl_source,
-                    's_sta_name': line.s_sta_name,
-                    'd_sta_name': line.d_sta_name,
-                    'drv_datetime': line.drv_datetime,
-                }
-                # line_id = md5("%(s_city_name)s-%(d_city_name)s-%(drv_datetime)s-%(bus_num)s-%(crawl_source)s" % line_id_args)
-                line_id = md5("%(s_city_name)s-%(d_city_name)s-%(drv_datetime)s-%(s_sta_name)s-%(d_sta_name)s-%(crawl_source)s" % line_id_args)
-                if line_id in t:
-                    t[line_id].update(**{"left_tickets": left_tickets, 'full_price': full_price, "refresh_datetime": now})
-                if line_id == line.line_id and int(left_tickets) and bus_num == line.bus_num:
-                    update_attrs = {"left_tickets": left_tickets, 'full_price': full_price, "refresh_datetime": now}
-            except:
-                pass
 
-        result_info = {}
+        ft = Line.objects.filter(s_city_name=line.s_city_name, d_city_name=line.d_city_name, drv_date=line.drv_date)
+        db_lines = {x.line_id: x for x in ft}
+
+        update_attrs = {}
+        for tr_o in soup.select("table #selbuy"):
+            td_lst = tr_o.find_all('td')
+            if len(td_lst) < 2:
+                continue
+            index_tr = lambda idx: td_lst[idx].text.strip().decode("utf-8")
+            full_price=float(index_tr(6))
+            left_tickets=int(index_tr(10))
+            drv_date, drv_time = line.drv_date, index_tr(1)
+            if u"流水" in drv_time:
+                continue
+            drv_datetime=dte.strptime("%s %s" % (drv_date, drv_time), "%Y-%m-%d %H:%M")
+            line_id_args = {
+                's_city_name': line.s_city_name,
+                'd_city_name': line.d_city_name,
+                'crawl_source': line.crawl_source,
+                's_sta_name': line.s_sta_name,
+                'd_sta_name': line.d_sta_name,
+                'drv_datetime': drv_datetime,
+            }
+            line_id = md5("%(s_city_name)s-%(d_city_name)s-%(drv_datetime)s-%(s_sta_name)s-%(d_sta_name)s-%(crawl_source)s" % line_id_args)
+            if line_id in db_lines:
+                db_lines[line_id].update(**{"left_tickets": left_tickets, 'full_price': full_price, "refresh_datetime": now})
+            if line_id == line.line_id:
+                update_attrs = {"left_tickets": left_tickets, 'full_price': full_price, "refresh_datetime": now}
+
         if not update_attrs:
-            result_info.update(result_msg="no line info", update_attrs={
-                               "left_tickets": 0, "refresh_datetime": now})
+            result_info.update(result_msg="no line info", update_attrs={"left_tickets": 0, "refresh_datetime": now})
         else:
             result_info.update(result_msg="ok", update_attrs=update_attrs)
         return result_info
@@ -282,7 +265,6 @@ class Flow(BaseFlow):
         is_login = rebot.test_login_status()
 
         # 登录验证码
-        # rebot_log.info(is_login)
         if not is_login:
             for x in xrange(3):
                 if rebot.login() == "OK":
@@ -304,12 +286,10 @@ class Flow(BaseFlow):
                 # v = vcode_glcx(cookies)
                 # url = 'http://www.0000369.cn/buytks!checkRand.action?rand=%s' % v[0]
                 # r = requests.get(url, headers=headers, cookies=cookies)
-                # rebot_log.info(r.content)
                 pay_url = 'http://www.0000369.cn/order!toAfformOrder.action'
                 data = order.extra_info
                 r = requests.post(pay_url, data=urllib.urlencode(data), headers=headers, cookies=cookies, allow_redirects=False)
                 soup = bs(r.content, 'lxml')
-                # rebot_log.info(soup)
                 content = r.content.replace('src="/', 'src="http://www.0000369.cn/').replace('href="/', 'href="http://www.0000369.cn/')
                 trade_no = soup.find_all('label')[0].get_text()
                 pay_money = float(soup.find_all('label')[1].get_text())
