c33cb801262d3efa402a4a942cc76871f7e70233
luocky <luojunping@qq.com>
2016-09-12 19:50:29 +0800

    szky

--- app/models.py
+++ app/models.py
@@ -5082,7 +5082,7 @@ class SzkyWebRebot(Rebot):
                     }):
                 cnt = d["count"]
                 phone = d["_id"]["phone"]
-                if cnt >= 7:
+                if cnt >= 10:
                     droped.add(phone)
         tele = random.choice(list(all_accounts - droped))
         return cls.objects.get(telephone=tele)
