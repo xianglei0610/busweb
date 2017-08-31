90a7d50227e94ea32270c9b60fd7b2cac5c89683
xiangleilei <leilei.xiang@lantaiyuan.com>
2017-08-31 11:12:23 +0800

    update .gitignore

--- sms/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding:utf-8 -*-
-"""
-sms
-"""
-from sms import send_msg
-
--- sms/config.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-URL = 'http://sdk4report.eucp.b2m.cn:8080/sdk/SDKService?wsdl'
-SERIAL_NO = '6SDK-EMY-6688-KEQMK'
-KEY = '123456987'
-PASSWORD = '429585'
